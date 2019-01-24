6152bc0e8024a1eb8a464b7b4c7f9aa66d4d08e1
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2019-01-24 22:55:26 +0300

    Sorted options, improved code comments.
    menuLeftPadding option was removed.
    order option changed available values.
    menuDirection option was removed.
    titleVisibility option was added.
    menuInHeader option was added.

--- src/options.ts
+++ src/options.ts
@@ -2,72 +2,68 @@ import { Menu } from "electron";
 
 export interface TitlebarConstructorOptions {
   /**
-   * The icon shown on the left side of the title bar.
-   */
-  icon?: string;
-  /**
-   * Style of the icons.
-   * You can create your custom style using `HTMLStyleElements`
-   */
-  iconsStyle?: HTMLStyleElement;
-  /**
-   * The shadow of the titlebar.
-   * This property is similar to box-shadow
-   */
-  shadow?: string;
-  /**
-   * The menu to show in the title bar.
-   * You can use `Menu` or not add this option and the menu created in the main process will be taken.
-   */
-  menu?: Menu | null;
-  /**
-   * Define whether or not you can drag the window by holding the click on the title bar.
-   * *The default value is true*
-   */
-  drag?: boolean;
-  /**
-   * Define if the minimize window button is displayed.
-   * *The default value is true*
-   */
-  minimizable?: boolean;
-  /**
-   * Define if the maximize and restore window buttons are displayed.
-   * *The default value is true*
-   */
-  maximizable?: boolean;
-  /**
-   * Define if the close window button is displayed.
-   * *The default value is true*
+   * Define if the close window button is displayed, default value is `true`.
    */
   closeable?: boolean;
   /**
-   * Set the order of the elements on the title bar.
-   * *The default value is normal*
+   * Define whether or not you can drag the window by holding the click on the title bar, default value is `true`.
    */
-  order?: "normal" | "reverse" | "firstButtons";
+  drag?: boolean;
   /**
-   * Set horizontal alignment of the window title.
-   * *The default value is center*
+   * The icon shown on the left side of the title bar. Default value is `null`.
    */
-  titleHorizontalAlignment?: "left" | "center" | "right";
+  icon?: string;
   /**
-   * The background color when the mouse is over the item
+   * Style of the icons. You can create your custom style using `HTMLStyleElements`.
+   * Default value is `Themebar.win`.
    */
-  menuItemHoverColor?: string;
+  iconsStyle?: HTMLStyleElement;
   /**
-   * The background color of menu panel.
+   * Define if the maximize and restore window buttons are displayed, default value is `true`.
+   */
+  maximizable?: boolean;
+  /**
+   * The menu to show in the title bar. if `null` - menu not be displayed.
+   * You can use `Menu` or not add this option and the menu created in the main process will be taken.
+   * Default value `be getted by electron`.
+   */
+  menu?: Menu | null;
+  /**
+   * Menu panel background color, default value is `rgba(0, 0, 0, .08)`.
+   */
+  menuBackgroundColor: string;
+  /**
+   * Position menu, if `true` menu be displayed in header, if false in bottom of title-bar.
+   * Important! True value caused center horizontal align for title, and menu be refresh to `false` if your options order to inverted!
+   * Default value of it property is `false`.
+   */
+  menuInHeader: boolean;
+  /**
+   * Menu button hover color, default value is `rgba(0, 0, 0, .14)`.
+   */
+  menuItemHoverColor: string;
+  /**
+   * Menu separator color, default value is `rgba(0, 0, 0, .29)`.
+   */
+  menuSeparatorColor: string;
+  /**
+   * Define if the minimize window button is displayed, default value is `true`.
+   */
+  minimizable?: boolean;
+  /**
+   * Order of the elements on the title bar, default value is `default`.
    */
   menuBackgroundColor?: string;
   /**
-   * The color of menu separator.
+   * The shadow of the titlebar. This property is similar to box-shadow.
    */
-  menuSeparatorColor?: string;
+  shadow?: string;
   /**
-   * Menu padding from left window border.
+   * Horizontal alignment of the window title, default value is `center`.
    */
-  menuLeftPadding?: string;
+  titleHorizontalAlignment: "left" | "center" | "right";
   /**
-   * Menu direction, supported left and right.
+   * Title text visibility, if `true` - title not be displayed in title-bar and not be created, default value is `true`.
    */
-  menuDirection: "left" | "right";
+  titleVisibility: boolean;
 }
