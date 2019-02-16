2620ae6bec2d2ee8bb4e61464ed6492a46c930e1
Alex Torres <alextorressk@gmail.com>
2019-02-15 19:40:04 -0500

    Changed Example File

--- example/renderer.js
+++ example/renderer.js
@@ -1,38 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var __1 = require("..");
-var electron_1 = require("electron");
-var Menu = electron_1.remote.Menu;
-var titlebar = new __1.Titlebar({
+new __1.Titlebar({
     backgroundColor: __1.Color.fromHex('#37474f'),
     icon: './images/icon.svg',
     shadow: true
 });
-titlebar.updateMenu(Menu.buildFromTemplate([
-    {
-        label: 'Set&tings',
-        click: function () { return alert('click settings'); }
-    },
-    {
-        label: '&File',
-        submenu: [
-            {
-                label: 'File item 1',
-                click: function () { return alert('click file item 1'); }
-            },
-            {
-                label: 'File item 2',
-                type: 'checkbox',
-                click: function () { return alert('click file item 2'); }
-            },
-            {
-                label: 'File item 3',
-                submenu: [
-                    {
-                        label: 'Submenu 1'
-                    }
-                ]
-            }
-        ]
-    }
-]));
--- example/renderer.ts
+++ example/renderer.ts
@@ -1,38 +1,7 @@
 import { Titlebar, Color } from '..';
-import { remote } from 'electron';
-const Menu = remote.Menu;
 
-const titlebar = new Titlebar({
+new Titlebar({
     backgroundColor: Color.fromHex('#37474f'),
     icon: './images/icon.svg',
     shadow: true
 });
-
-titlebar.updateMenu(Menu.buildFromTemplate([
-    {
-        label: 'Set&tings',
-        click: () => alert('click settings'),
-    },
-    {
-        label: '&File',
-        submenu: [
-            {
-                label: 'File item 1',
-                click: () => alert('click file item 1')
-            },
-            {
-                label: 'File item 2',
-                type: 'checkbox',
-                click: () => alert('click file item 2')
-            },
-            {
-                label: 'File item 3',
-                submenu: [
-                    {
-                        label: 'Submenu 1'
-                    }
-                ]
-            }
-        ]
-    }
-]));
