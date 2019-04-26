42c413dd1d95688b1ac39d7df1b90af1dee06e00
Alex Torres <alextorressk@gmail.com>
2019-04-26 18:18:00 -0500

    Bug fixed #33

--- src/themebar.ts
+++ src/themebar.ts
@@ -56,7 +56,7 @@ export class Themebar extends ThemingRegistry {
                 line-height: 22px;
                 height: 22px;
                 display: flex;
-                z-index: 1000;
+                z-index: 99999;
             }
         
             .titlebar.windows, .titlebar.linux {
@@ -175,7 +175,7 @@ export class Themebar extends ThemingRegistry {
                 justify-content: flex-end;
                 white-space: nowrap;
                 box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
-                z-index: 2000;
+                z-index: 99999;
             }
             
             .menubar-menu-container:focus {
