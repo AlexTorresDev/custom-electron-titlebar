cf46cbd455ada167421c58d5cf4c03b057ece106
Alex Torres <alextorressk@gmail.com>
2023-02-16 09:52:31 -0500

    Change example code

--- example/main.js
+++ example/main.js
@@ -1,192 +1,54 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path');
-const { setupTitlebar, attachTitlebarToWindow } = require('custom-electron-titlebar/main');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { setupTitlebar, attachTitlebarToWindow } = require('custom-electron-titlebar/main')
 
-// setup the titlebar main process
-setupTitlebar();
+// Setup the titlebar
+setupTitlebar()
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    titleBarStyle: 'hidden',
-    frame: false, // needed if process.versions.electron < 14
-    webPreferences: {
-      sandbox: false,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  });
+function createWindow() {
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: false,
+		titleBarStyle: 'hidden',
+		// titleBarOverlay: true,
+		webPreferences: {
+			sandbox: false,
+			preload: path.join(__dirname, 'preload.js')
+		}
+	})
 
-  const menu = Menu.buildFromTemplate(exampleMenuTemplate());
-  Menu.setApplicationMenu(menu);
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools()
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  //attach fullscreen(f11 and not 'maximized') && focus listeners
-  attachTitlebarToWindow(mainWindow);
+	// Attach listeners
+	attachTitlebarToWindow(mainWindow)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+	createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  })
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+	if (process.platform !== 'darwin') app.quit()
 })
 
-const exampleMenuTemplate = () => [
-  {
-    label: "Simple O&ptions",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit()
-      },
-      {
-        label: "Radio1",
-        type: "radio",
-        checked: true
-      },
-      {
-        label: "Radio2",
-        type: "radio",
-      },
-      {
-        label: "Check&box1",
-        type: "checkbox",
-        checked: true,
-        click: (item) => {
-          console.log("item is checked? " + item.checked);
-        }
-      },
-      { type: "separator" },
-      {
-        label: "Che&ckbox2",
-        type: "checkbox",
-        checked: false,
-        click: (item) => {
-          console.log("item is checked? " + item.checked);
-        }
-      }
-    ]
-  },
-  {
-    label: "A&dvanced Options",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit()
-      },
-      {
-        label: "Radio1",
-        type: "radio",
-        checked: true
-      },
-      {
-        label: "Radio2",
-        type: "radio",
-      },
-      {
-        label: "Checkbox1",
-        type: "checkbox",
-        checked: true,
-        click: (item) => {
-          console.log("item is checked? " + item.checked);
-        }
-      },
-      { type: "separator" },
-      {
-        label: "Checkbox2",
-        type: "checkbox",
-        checked: false,
-        click: (item) => {
-          console.log("item is checked? " + item.checked);
-        }
-      },
-      {
-        label: "Radio Test",
-        submenu: [
-          {
-            label: "S&ample Checkbox",
-            type: "checkbox",
-            checked: true
-          },
-          {
-            label: "Radio1",
-            checked: true,
-            type: "radio"
-          },
-          {
-            label: "Radio2",
-            type: "radio"
-          },
-          {
-            label: "Radio3",
-            type: "radio"
-          },
-          { type: "separator" },
-          {
-            label: "Radio1",
-            checked: true,
-            type: "radio"
-          },
-          {
-            label: "Radio2",
-            type: "radio"
-          },
-          {
-            label: "Radio3",
-            type: "radio"
-          }
-        ]
-      },
-      {
-        label: "zoomIn",
-        role: "zoomIn"
-      },
-      {
-        label: "zoomOut",
-        role: "zoomOut"
-      },
-      {
-        label: "Radio1",
-        type: "radio"
-      },
-      {
-        label: "Radio2",
-        checked: true,
-        type: "radio"
-      },
-    ]
-  },
-  {
-    label: "&View",
-    submenu: [
-      { role: "reload" },
-      { role: "forceReload" },
-      { type: "separator" },
-      { role: "zoomIn" },
-      { role: "zoomOut" },
-      { role: "resetZoom" },
-      { role: "toggleDevTools" }
-    ],
-  }
-];
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
--- example/preload.js
+++ example/preload.js
@@ -1,27 +1,24 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-const { Titlebar, Color } = require('custom-electron-titlebar');
-const path = require('path');
-
-let titlebar;
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const { CustomTitlebar, TitlebarColor } = require('custom-electron-titlebar')
 
 window.addEventListener('DOMContentLoaded', () => {
-  titlebar = new Titlebar({
-    backgroundColor: Color.fromHex("#262626"),
-    //itemBackgroundColor: Color.fromHex("#ffffff"),
-    svgColor: Color.WHITE,
-    icon: path.join(__dirname, '/assets/images', '/icon.svg'),
-    //menuPosition: 'bottom',
-    //menu: null // = do not automatically use Menu.applicationMenu
-    menuTransparent: 80,
-  })
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector)
+		if (element) element.innerText = text
+	}
 
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+	for (const type of ['chrome', 'node', 'electron']) {
+		replaceText(`${type}-version`, process.versions[type])
+	}
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+	// eslint-disable-next-line no-new
+	new CustomTitlebar({
+		backgroundColor: TitlebarColor.fromHex('#2f3241')
+	})
 })
