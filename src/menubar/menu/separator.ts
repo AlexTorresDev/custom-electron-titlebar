e1ffba1c96bd6c020ff0da5eefebf344400f4dc1
Alex Torres <alextorressk@gmail.com>
2023-04-13 11:55:11 -0500

    Added separator and submenu element

---
+++ src/menubar/menu/separator.ts
@@ -0,0 +1,26 @@
+import { MenuItem } from "electron";
+import { CETMenuItem, IMenuStyle } from "./item";
+import { IMenuOptions } from ".";
+import { $, append } from "base/common/dom";
+
+export class CETSeparator extends CETMenuItem {
+
+  private separatorElement?: HTMLElement
+
+  constructor(item: MenuItem, options: IMenuOptions) {
+    super(item, options)
+    console.log('Generate separator', item)
+  }
+
+  render(container: HTMLElement) {
+    if (container) {
+      this.separatorElement = append(container, $('a.cet-action-label.separator', { role: 'presentation' }))
+    }
+  }
+
+  style(style: IMenuStyle) {
+    if (this.separatorElement && style.separatorColor)
+      this.separatorElement.style.borderBottomColor = style.separatorColor.toString()
+  }
+
+}
