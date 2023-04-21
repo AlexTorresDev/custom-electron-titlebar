6be3b546510181da2df5cd0a5f3381eb5ffef9b5
Alex Torres <alextorressk@gmail.com>
2023-04-20 23:44:54 -0500

    Modify titlebar height

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 import { Color } from "base/common/color";
-import { isMacintosh } from "base/common/platform";
+import { isMacintosh, isWindows } from "base/common/platform";
 
 export const INACTIVE_FOREGROUND_DARK = Color.fromHex('#222222')
 export const ACTIVE_FOREGROUND_DARK = Color.fromHex('#333333')
@@ -10,7 +10,7 @@ export const DEFAULT_ITEM_SELECTOR = Color.fromHex('#0000001F')
 export const IS_MAC_BIGSUR_OR_LATER = isMacintosh && parseInt(process.getSystemVersion().split(".")[0]) >= 11
 export const BOTTOM_TITLEBAR_HEIGHT = '60px'
 export const TOP_TITLEBAR_HEIGHT_MAC = IS_MAC_BIGSUR_OR_LATER ? '28px' : '22px'
-export const TOP_TITLEBAR_HEIGHT_WIN = '30px'
+export const TOP_TITLEBAR_HEIGHT_WIN = '32px'
 
 export const WINDOW_MIN_WIDTH = 400
 export const WINDOW_MIN_HEIGHT = 270
@@ -30,6 +30,37 @@ export function mnemonicMenuLabel(label: string, forceDisableMnemonics?: boolean
 	}
 
 	return label.replace(/&&|&/g, m => m === '&' ? '&&' : '&');
+}
+
+/**
+ * Handles mnemonics for buttons. Depending on OS:
+ * - Windows: Supported via & character (replace && with & and & with && for escaping)
+ * - Linux: Supported via _ character (replace && with _)
+ * - macOS: Unsupported (replace && with empty string)
+ */
+export function mnemonicButtonLabel(label: string, forceDisableMnemonics?: boolean): string {
+	if (isMacintosh || forceDisableMnemonics) {
+		return label.replace(/\(&&\w\)|&&/g, '');
+	}
+
+	if (isWindows) {
+		return label.replace(/&&|&/g, m => m === '&' ? '&&' : '&');
+	}
+
+	return label.replace(/&&/g, '_');
+}
+
+export function cleanMnemonic(label: string): string {
+	const regex = MENU_MNEMONIC_REGEX;
+
+	const matches = regex.exec(label);
+	if (!matches) {
+		return label;
+	}
+
+	const mnemonicInText = !matches[1];
+
+	return label.replace(regex, mnemonicInText ? '$2$3' : '').trim();
 }
 
 export function parseAccelerator(accelerator: Electron.Accelerator | string): string {
