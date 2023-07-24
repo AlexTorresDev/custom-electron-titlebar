60b0876da3313e5d6ca37a58b2b4b1515a2e7fa7
Alex Torres <alextorressk@gmail.com>
2023-07-24 10:01:24 -0500

    chore: :arrow_up: Update dependencies

--- revert.sh
+++ revert.sh
@@ -1,3 +1,7 @@
-# Reverts ./dist path changes created in pipeline by package publish to NPM and deletes postversion script in package.json of NPM package
+#!/bin/bash
+
+# Reverts ./dist path changes created in pipeline by package publish to NPM
 sed -i -e '/"types":/,/},/{s|"\./|"./dist/|g}' -e '/"exports":/,/},/{/\"\.\/main\":/! s|"\./|"./dist/|g; s|"\./main/index.js|"./dist/main/index.js|g}' ../package.json
+
+# Deletes postversion script in package.json of NPM package
 sed -i '/"postversion":/d' package.json
